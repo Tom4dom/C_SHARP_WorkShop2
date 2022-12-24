a614fd78d1ebb36a792259d17312a74516060188
Tom4dom <120015368+Tom4dom@users.noreply.github.com>
2022-12-24 17:13:32 +0300

    Homework_Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 10: Напишите программу, которая 
+// 1. принимает на вход трёхзначное число и
+// 2. на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Method(int arg0)
+{
+    int result = (arg0 % 100) / 10;
+    return result;
+}
+
+
+Console.WriteLine("Input a three-digit number: ");
+int usrNmbr = Convert.ToInt32(Console.ReadLine());
+
+if (usrNmbr <= 999)
+{
+    int secondDigit = Method(usrNmbr);
+    Console.WriteLine($"{usrNmbr} -> {secondDigit}");
+}
+else
+{
+    Console.WriteLine("Incorrect number!");
+}
+
+
+// if (usrNmbr <= 999)
+// {
+//     int secondDigit = (usrNmbr % 100) / 10;
+//     Console.WriteLine($" {usrNmbr} -> {secondDigit}");
+// }
+// else
+// {
+//     Console.WriteLine("Incorrect number!");
+// }
+
+
+
+
+
+
