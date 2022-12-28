e87984a009509c959e887fdc5fe89506930786c9
Tom4dom <120015368+Tom4dom@users.noreply.github.com>
2022-12-28 17:26:09 +0300

    HomeworkTask21_reworked

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -4,3 +4,70 @@
 // 23432 -> да
 // 12821 -> да
 
+
+
+
+
+
+
+
+
+
+
+// using System;
+// public class ReverseExample
+// {
+//     public static void Main(string[] args)
+//     {
+//         int n, reverse = 0, rem;
+//         Console.Write("Enter a number: ");
+//         n = int.Parse(Console.ReadLine());
+//         while (n != 0)
+//         {
+//             rem = n % 10;
+//             reverse = reverse * 10 + rem;
+//             n /= 10;
+//             if (n == reverse)
+//         {
+//             Console.Write("palindrom!");
+//         }
+//         }
+
+
+//         Console.Write("Reversed Number: " + reverse);
+//     }
+// }
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Reverse_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, temp, remainder, reverse = 0;
+            Console.Write("Enter an integer ");
+            num = int.Parse(Console.ReadLine());
+            temp = num;
+            while (num > 0)
+            {
+                remainder = num % 10;
+                reverse = reverse * 10 + remainder;
+                num /= 10;
+            }
+            if (temp == reverse)
+            {
+                Console.WriteLine($"{temp} is a palindrome");
+            }
+            Console.WriteLine("Given number is = {0}", temp);
+            Console.WriteLine("Its reverse is = {0}", reverse);
+        }
+    }
+}
--- Task21/Program.cs
+++ Task21/Program.cs
@@ -4,37 +4,35 @@
 // A (7,-5,0); B(1,-1,9)  -> 11.53
 
 
-Console.WriteLine("Enter point A coordinates: ");
-Console.Write("AX: ");
-int ax = Convert.ToInt32(Console.ReadLine());
-Console.Write("AY: ");
-int ay = Convert.ToInt32(Console.ReadLine());
-Console.Write("AZ: ");
-int az = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Enter point B coordinates: ");
-Console.Write("BX: ");
-int bx = Convert.ToInt32(Console.ReadLine());
-Console.Write("BY: ");
-int by = Convert.ToInt32(Console.ReadLine());
-Console.Write("BZ: ");
-int bz = Convert.ToInt32(Console.ReadLine());
-
-double resultRes = Math.Round(Math.Sqrt((ax - bx) * (ax - bx) + (ay - by) * (ay - by) + (az - bz) * (az - bz)), 2, MidpointRounding.ToZero);
-
-Console.WriteLine(resultRes);
+int InsertPositon(string text)
+{
+    Console.WriteLine(text);
+    int pos = Convert.ToInt32(Console.ReadLine());
+    return pos;
+}
 
 
+double LenghtLine(int xpos1, int xpos2, int ypos1, int ypos2, int zpos1, int zpos2)
+{
+    int temp = (xpos1 - xpos2) * (xpos1 - xpos2) + (ypos1 - ypos2) * (ypos1 - ypos2) + (zpos1 - zpos2) * (zpos1 - zpos2);
+    return Math.Sqrt(temp);
+}
 
 
+int x1 = InsertPositon("Enter the coordinate of the point A (X-axis): ");
+int y1 = InsertPositon("Enter the coordinate of the point A (Y-axis): ");
+int z1 = InsertPositon("Enter the coordinate of the point A (Z-axis): ");
+
+int x2 = InsertPositon("Enter the coordinate of the point B (X-axis): ");
+int y2 = InsertPositon("Enter the coordinate of the point B (Y-axis): ");
+int z2 = InsertPositon("Enter the coordinate of the point B (Z-axis): ");
 
 
+double res = LenghtLine(x1, x2, y1, y2, z1, z2);
 
+double resRound = Math.Round(res, 2, MidpointRounding.ToZero);
 
-
-
-
-
+Console.WriteLine($"The distance between points A and B is {resRound}");
 
 
 
@@ -44,29 +42,27 @@ Console.WriteLine(resultRes);
 
 
 
-// int InsertPositon(string text)
-// {
-// Console.WriteLine(text);
-// int pos = Convert.ToInt32(Console.ReadLine());
-// return pos;
-// }
 
-// int x1 = InsertPositon("Введите координату Х первой точки:");
-// int y1 = InsertPositon("Введите координату Y первой точки:");
-// int y1 = InsertPositon("Введите координату Z первой точки:");
 
-// int x2 = InsertPositon("Введите координату Х второй точки:");
-// int y2 = InsertPositon("Введите координату Y второй точки:");
-// int y2 = InsertPositon("Введите координату Z второй точки:");
 
-// double LenghtLine(int xpos1, int xpos2, int ypos1, int ypos2)
-// {
-// int temp = (xpos1-xpos2)*(xpos1-xpos2) + (ypos1-ypos2)*(ypos1-ypos2);
-// return Math.Sqrt(temp);
-// }
+// Console.WriteLine("Enter point A coordinates: ");
+// Console.Write("AX: ");
+// int ax = Convert.ToInt32(Console.ReadLine());
+// Console.Write("AY: ");
+// int ay = Convert.ToInt32(Console.ReadLine());
+// Console.Write("AZ: ");
+// int az = Convert.ToInt32(Console.ReadLine());
 
-// double res = LenghtLine(x1,x2,y1,y2);
+// Console.WriteLine("Enter point B coordinates: ");
+// Console.Write("BX: ");
+// int bx = Convert.ToInt32(Console.ReadLine());
+// Console.Write("BY: ");
+// int by = Convert.ToInt32(Console.ReadLine());
+// Console.Write("BZ: ");
+// int bz = Convert.ToInt32(Console.ReadLine());
 
-// double resRound = Math.Round(res,2,MidpointRounding.ToZero);
+// double resultRes = Math.Round(Math.Sqrt((ax - bx) * (ax - bx) + (ay - by) * (ay - by) + (az - bz) * (az - bz)), 2, MidpointRounding.ToZero);
 
-// Console.WriteLine($"Растояние между точками равно :{resRound}");
+// Console.WriteLine(resultRes);
+
+
