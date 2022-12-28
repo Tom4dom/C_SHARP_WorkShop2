79feccf6330ec9764e6e803263b80e8ca2ab16b0
Tom4dom <120015368+Tom4dom@users.noreply.github.com>
2022-12-28 18:44:20 +0300

    HomeworkTask19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -4,70 +4,28 @@
 // 23432 -> да
 // 12821 -> да
 
+// Console.WriteLine("Enter an integer: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter an integer: ");
+int num = Convert.ToInt32(Console.ReadLine());
 
 
-
-
-
-
-
-
-
-
-// using System;
-// public class ReverseExample
-// {
-//     public static void Main(string[] args)
-//     {
-//         int n, reverse = 0, rem;
-//         Console.Write("Enter a number: ");
-//         n = int.Parse(Console.ReadLine());
-//         while (n != 0)
-//         {
-//             rem = n % 10;
-//             reverse = reverse * 10 + rem;
-//             n /= 10;
-//             if (n == reverse)
-//         {
-//             Console.Write("palindrom!");
-//         }
-//         }
-
-
-//         Console.Write("Reversed Number: " + reverse);
-//     }
-// }
-
-
-
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace Reverse_Number
+int reverse = 0;
+int temp = num;
+while (num != 0)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int num, temp, remainder, reverse = 0;
-            Console.Write("Enter an integer ");
-            num = int.Parse(Console.ReadLine());
-            temp = num;
-            while (num > 0)
-            {
-                remainder = num % 10;
-                reverse = reverse * 10 + remainder;
-                num /= 10;
-            }
-            if (temp == reverse)
-            {
-                Console.WriteLine($"{temp} is a palindrome");
-            }
-            Console.WriteLine("Given number is = {0}", temp);
-            Console.WriteLine("Its reverse is = {0}", reverse);
-        }
-    }
+    int remainder = num % 10;
+    reverse = reverse * 10 + remainder;
+    num /= 10;
+}
+if (temp == reverse)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Congratulation! The number {temp} is a palindrome!");
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine($"Sorry! The number {temp} is NOT a palindrome!");
 }
